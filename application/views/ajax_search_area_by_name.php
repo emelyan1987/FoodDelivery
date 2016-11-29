2daa23fa867279d1ad723b75259745e23875ff2e
emelyan1987 <emelyan.a@outlook.com>
2016-11-29 04:25:30 +0200

    Double quote issue

--- application/views/ajax_search_area_by_name.php
+++ application/views/ajax_search_area_by_name.php
@@ -2,7 +2,7 @@
     <?php
         foreach($area_list as $cu => $list):
         ?>
-        <li onclick="citychangeintxt('<?php echo htmlspecialchars($list->city_name); ?>,<?php echo htmlspecialchars($list->name); ?>','<?php echo $divid; ?>','<?php echo $list->id; ?>')"><span ><?php echo $list->city_name; ?>,<?php echo $list->name; ?></span></li>
+        <li onclick="citychangeintxt('<?php echo htmlspecialchars($list->city_name, ENT_QUOTES, 'UTF-8'); ?>,<?php echo htmlspecialchars($list->name, ENT_QUOTES, 'UTF-8'); ?>','<?php echo $divid; ?>','<?php echo $list->id; ?>')"><span ><?php echo $list->city_name; ?>,<?php echo $list->name; ?></span></li>
 
         <?php
             endforeach;
