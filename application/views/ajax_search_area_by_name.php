524f741ceef1c0ef42add95c0f636281ab676c4a
emelyan1987 <emelyan.a@outlook.com>
2016-11-29 04:19:49 +0200

    Fixed double quote issue on Where Do You Eat?

--- application/views/ajax_search_area_by_name.php
+++ application/views/ajax_search_area_by_name.php
@@ -1,12 +1,11 @@
 <ul>
-	<?php
-	foreach($area_list as $cu => $list):
-	?>
-        <li onclick='citychangeintxt("<?php echo $list->city_name; ?>,<?php echo $list->name; ?>","<?php echo $divid; ?>","<?php echo $list->id; ?>")'><span ><?php echo $list->city_name; ?>,<?php echo $list->name; ?></span></li>
-                                            
     <?php
-    endforeach;
-    ?>     
+        foreach($area_list as $cu => $list):
+        ?>
+        <li onclick='citychangeintxt("<?php echo htmlspecialchars($list->city_name); ?>,<?php echo htmlspecialchars($list->name); ?>","<?php echo $divid; ?>","<?php echo $list->id; ?>")'><span ><?php echo $list->city_name; ?>,<?php echo $list->name; ?></span></li>
 
-    
+        <?php
+            endforeach;
+    ?>  
+
 </ul>
